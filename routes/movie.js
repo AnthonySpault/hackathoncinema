743d6a7c3f4c3645d65e37c6eff7ea0ac00e3a05
Nathael Arki <nathael.arki@supinternet.fr>
2018-12-20 09:35:48 +0100

    get best movie last month

--- routes/movie.js
+++ routes/movie.js
@@ -57,4 +57,52 @@ router.get('', (req, res) => {
     })
 })
 
+router.get('/qualifiedMovie', (req, res) => {
+    const date = new Date()
+
+    const month = req.query.month || date.getMonth()
+
+    const firstDay = new Date(date.getFullYear(), month, 1)
+    const lastDay = new Date(date.getFullYear(), month + 1, 0)
+
+    Movie.aggregate([
+        {
+            $addFields: {
+                date: {
+                    $dateFromString: {
+                        dateString: '$date_release'
+                    }
+                }
+            }
+        },
+        {
+            $match: {
+                date: {
+                    $gte: new Date(firstDay),
+                    $lt: new Date(lastDay)
+                }
+            }
+        }
+    ]).then((movies, errors) => {
+        Vote.find({movie_id: { $in: movies.map((movie) => movie._id) }})
+        .then((votes, errors) => {
+
+            return res.sjson({
+                status: 200,
+                data: movies.map((movie) => {
+
+                    const filteredVotes = votes.filter((vote) => vote.movie_id.toString() === movie._id.toString())
+
+                    const grade = filteredVotes.reduce((a, b) => a + b.grade, 0) / filteredVotes.length
+
+                    return {
+                        ...movie,
+                        grade,
+                    }
+                }).sort((a, b) => a.grade < b.grade)[0]
+            })
+        })
+    })
+})
+
 export default router
